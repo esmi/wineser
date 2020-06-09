4eb69b509915168bc1d248331787a2ee52068a01
esmi <evansmile@gmail.com>
2020-06-09 15:21:09 +0800

    use heredoc for wineCom.reg/mkcom.reg

--- mkcom.sh
+++ mkcom.sh
@@ -4,7 +4,17 @@
 echo "Run vserp.sh to Create /dev/ttyUSBLocal and /dev/ttyUSBRemote"
 ./vserp.sh --create
 echo "Run regedit to create com1, com2 registry."
-wine regedit ./wineCom.reg
+
+cat <<HERE > mkcom.reg
+REGEDIT4
+
+[HKEY_LOCAL_MACHINE\HARDWARE\DEVICEMAP\SERIALCOMM]
+"COM1"="COM1"
+"COM2"="COM2"
+HERE
+
+wine regedit ./mkcom.reg
+
 #將com1,com2 連結至 "vserp.sh" 所產生的"tty"檔案
 if [[ -a $HOME/.wine/dosdevices/com1 && -a $HOME/.wine/dosdevices/com2 ]]; then
   echo "com1, com2 alreay exist!"
