67960c545263360fe2b19f883a85372c4839e4a3
esmi <evansmile@gmail.com>
2020-06-11 10:40:35 +0800

    fix "ref"

--- mkcom.sh
+++ mkcom.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # make virtual com port pair com1, com2.
-#ref: https://drive.google.com/file/d/1_Rd07q_CrOmX6iY4vl6fa1DzcqPtgESG/view?usp=sharing
+#ref: https://github.com/esmi/docs/blob/master/linux_modbus%E6%A8%A1%E6%93%AC%E5%99%A8%E6%B8%AC%E8%A9%A6%E7%92%B0%E5%A2%83.md
+
 echo "Run vserp.sh to Create /dev/ttyUSBLocal and /dev/ttyUSBRemote"
 ./vserp.sh --create
-
 
 echo "Run regedit to create com1, com2 registry."
 
